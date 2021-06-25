f2853529122fc5bf9ea081796238892e19b6f018
Youssouph Ndao <47628329+Youniesta@users.noreply.github.com>
2021-06-25 17:01:57 +0200

    Update Max86141_Init.ino

--- Max86141/examples/Max86141_Init/Max86141_Init.ino
+++ Max86141/examples/Max86141_Init/Max86141_Init.ino
@@ -14,8 +14,8 @@
 #include "MAX86141.h"
 
 //PD: PhotoDiode
-//#define PDsLED; // 2 PD - 1 LED
-#define PDLEDs; // 1 PD - 2 LEDs
+#define PDsLED; // 2 PDs - 1 LED
+//#define PDLEDs; // 1 PD - 2 LEDs
 //#define PDsLEDs; // 2 PDs - 3 LEDs
 
 static int spiClk = 1000000; // 8 MHz Maximum
@@ -38,6 +38,8 @@ float RED_AVG, IR_AVG, AMBIENT_AVG;
 
 MAX86141 pulseOx1;
 
+long startTime;
+long samplesTaken = 0; //Counter for calculating the Hz or read rate
 
 void setup() {
   Serial.begin(115200);
@@ -61,14 +63,12 @@ void setup() {
   pulseOx1.setDebug(true);
   #ifdef PDsLED
     int LedMode[]= {1/*LED1 (Sequence 1, 0-3)*/, 9/*DIRECT AMBIENT (Sequence 1, 4-7)*/};
-    pulseOx1.initialisation(2/*nb_ppg*/,LedMode/*LedMode*/,2/*Number of sequences*/,1/*Number of LEDs*/,0x10/*intensity_LEDs*/,4/*sample_average*/, 0x0B/*sample_rate*/,0x3/*pulse width*/,0x2/*ADC Range= 16uA*/,spiClk);
+    pulseOx1.initialisation(2/*nb_ppg*/,LedMode/*LedMode*/,2/*Number of sequences*/,1/*Number of LEDs*/,6/*intensity_LEDs*/,4/*sample_average*/, 0x0E/*sample_rate*/,0x3/*pulse width*/,0x2/*ADC Range= 16uA*/,spiClk);
   #endif
 
   #ifdef PDLEDs
    int LedMode[]={4/*LED1 and LED2 simultaneously (Sequence 1, 0-3)*/, 9/*DIRECT AMBIENT (Sequence 1, 4-7)*/};
-   //int LedMode[]={4/*LED1 and LED2 simultaneously*/};
-   pulseOx1.initialisation(1/*nb_ppg*/,LedMode/*LedMode*/,1/*Number of sequences*/,2/*Number of LEDs*/,0x7/*intensity_LEDs*/,1/*sample_average*/, 0x0E/*sample_rate*/,0x3/*pulse width*/,0x3/*ADC Range= 16uA*/,spiClk);
-   //pulseOx1.initialisation(1/*nb_ppg*/,LedMode/*LedMode*/,2/*Number of sequences*/,15/*intensity_LEDs*/,1/*sample_average*/, 0x0E/*sample_rate*/,0x3/*pulse width*/,0x3/*ADC Range= 16uA*/,spiClk);
+   pulseOx1.initialisation(1/*nb_ppg*/,LedMode/*LedMode*/,2/*Number of sequences*/,2/*Number of LEDs*/,15/*intensity_LEDs*/,1/*sample_average*/, 0x0E/*sample_rate*/,0x3/*pulse width*/,0x3/*ADC Range= 16uA*/,spiClk);
   #endif
 
   #ifdef PDsLEDs
@@ -86,23 +86,27 @@ void setup() {
   pulseOx1.write_reg(0x40, 0xFF);
 
   Serial.println(pulseOx1.read_reg(0x41));
-  Serial.println(pulseOx1.read_reg(0x41));
+  //Serial.println(pulseOx1.read_reg(0x41));
   delay(1000);
 
   pulseOx1.setDebug(false);
-    pulseOx1.signalData_ledSeq1A_PPG1.setStorage(pulseOx1.storage_array);
-
+  
+  // Set signal bufer size
+  pulseOx1.signalData_ledSeq1A_PPG1.setStorage(pulseOx1.storage_array);
 }
 
 
 void loop() {
-
+    uint8_t intStatus;
+    //Read Status
+    intStatus = pulseOx1.read_reg(REG_INT_STAT_1);
+      
+if ( intStatus & 0x80 ) { //A FULL RDY
   fifo_size = pulseOx1.read_reg(REG_FIFO_DATA_COUNT);
   //Serial.println("fifo-size :"+String(fifo_size));
   
   #ifdef PDsLED
-  if(fifo_size >= 4){
-  uint8_t dataBuf[3072]; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 2 number of sequences control (128*3*2*4)*2 = 3072 byte buffer
+  uint8_t dataBuf[3072]={0}; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 2 number of sequences control (128*3*2*4)*2 = 3072 byte buffer
   pulseOx1.device_data_read(dataBuf,fifo_size);
     Serial.println("LED 1 Seq 1 A - PPG1 channel : ");
     int ledSeq1A_PPG1 = pulseOx1.get_ledSeq1A_PPG1()[0];
@@ -135,14 +139,14 @@ void loop() {
     Serial.println("Direct ambient AVERAGE");
     int ambient_avg = (ledSeq1B_PPG1+ledSeq1B_PPG2)*0.5;
     Serial.println(ambient_avg);
-  }
+
+    //delayMicroseconds(2500);
   #endif
 
   #ifdef PDLEDs
-  if(fifo_size >= 2){
-  uint8_t dataBuf[3072]; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 2 number of sequences control (128*3*2*4)*2 = 3072 byte buffer
-  pulseOx1.device_data_read(dataBuf,fifo_size);
-    
+    uint8_t dataBuf[3072]={ 0 }; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 2 number of sequences control (128*3*2*4)*2 = 3072 byte buffer
+    pulseOx1.device_data_read(dataBuf,fifo_size);
+   
     Serial.println("LED 1 & LED 2 Seq 1 A - PPG1 channel : ");
     int ledSeq1A_PPG1 = pulseOx1.get_ledSeq1A_PPG1()[0];
     Serial.println(ledSeq1A_PPG1);
@@ -155,19 +159,19 @@ void loop() {
     Serial.println("TAG Direct ambient - PPG1 channel : ");
     int tagSeq1B_PPG1 = pulseOx1.get_tagSeq1B_PPG1()[0];
     Serial.println(tagSeq1B_PPG1);
-    
+
     ///////////// Add in buffer for SNR //////////
-  pulseOx1.signalData_ledSeq1A_PPG1.push_back(ledSeq1A_PPG1);
-  if(pulseOx1.signalData_ledSeq1A_PPG1.size()==SIZE){
-    Serial.println("SNR (dB): "+String(pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PPG1, SIZE)));
+    pulseOx1.signalData_ledSeq1A_PPG1.push_back(ledSeq1A_PPG1);
+    if(pulseOx1.signalData_ledSeq1A_PPG1.size()==SIZE){
+    //Serial.println("SNR (dB): "+String(pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PPG1, SIZE)));
+     Serial.println(pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PPG1, SIZE));
+
     pulseOx1.signalData_ledSeq1A_PPG1.clear();
-  }
-   }
+    }
   #endif
   
   #ifdef PDsLEDs
-  if(fifo_size >= 8){
-  uint8_t dataBuf[6144]; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 4 number of sequences control (128*3*2*4)*4 = 6144 byte buffer
+  uint8_t dataBuf[6144]={0}; ///128 FIFO samples, 2 channels PPG, 3 bytes/channel, 4 number of sequences control (128*3*2*4)*4 = 6144 byte buffer
   pulseOx1.device_data_read(dataBuf,fifo_size);   
     Serial.println("LED 1 Seq 1 A - PPG1 channel : ");
     int ledSeq1A_PPG1 = pulseOx1.get_ledSeq1A_PPG1()[0];
@@ -234,8 +238,10 @@ void loop() {
     int ambient_avg = (ledSeq2B_PPG1+ledSeq2B_PPG2)*0.5;
     Serial.println(ambient_avg);
     
-  }
+    //delayMicroseconds(2500);
   #endif
-
-  delay(50);
 }
+else {
+  //Serial.println("FIFO not A_full");
+}
+  }
